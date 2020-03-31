72b820b0c97f70198d2c0ef43b85fee1fd93b976
MartinGrue <gruebler.martin@outlook.de>
2020-03-31 20:35:46 +0200

    npm updates

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -44,7 +44,7 @@ namespace DatingApp
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
             .UseStartup<Startup>()
-            // .UseUrls("http://*:5000")
+            .UseUrls("http://*:5000")
             ;
     }
 }
